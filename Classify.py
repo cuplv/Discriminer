5bbebb17dc7ded1675438d58bfb51c35ddd1bb73
Saeid Tizpaz Niari <saeid.tizpazniari@colorado.edu>
2016-08-21 22:18:10 -0600

    upload source code and benchmarks

---
+++ Classify.py
@@ -0,0 +1,66 @@
+import numpy as np
+from sklearn import preprocessing, cross_validation, neighbors, tree
+import pandas as pd
+import random
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.utils import shuffle
+from sklearn import decomposition
+
+filename = input("Enter the name of your input data set (.csv) without file type: ")
+kfolds = input("Please enter the number of random folds for cross-validation step (default is 20)? ")
+if(kfolds == ""):
+    kfolds_numbers = 20
+else:
+    try:
+        kfolds_numbers = int(kfolds)
+    except ValueError:
+        print("That's not an int!")
+max_depth_tree = input("Please enter the maximum depth of tree (do not specify any number if default value of algorithm is the best)? ")
+if(max_depth_tree == ""):
+    max_depth_tree_num = None
+else:
+    try:
+        max_depth_tree_num = int(max_depth_tree)
+    except ValueError:
+        print("That's not an int!")
+
+df = pd.read_csv("Classification_input/" + filename + ".csv",index_col = 'id')
+header = list(df.columns.values)
+header.remove('label')
+header.remove('weight')
+
+
+X = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+X, y = shuffle(X, y, random_state=0)
+
+accuracy_max = 0
+
+for i in range(3):
+    kf = cross_validation.KFold(len(X), n_folds=kfolds_numbers, shuffle=True, random_state=None)
+    for train_index, test_index in kf:
+    #    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.2)
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        
+        train_weights = X_train[:,-1]
+        X_train = np.delete(X_train,-1,1)
+        
+        test_weights = X_test[:,-1]
+        X_test = np.delete(X_test,-1,1)
+        
+        clf_temp = DecisionTreeClassifier(criterion='gini',splitter='best',max_depth=max_depth_tree_num)
+        clf_temp.fit(X_train,y_train,train_weights)
+        accuracy = clf_temp.score(X_test,y_test,test_weights)
+        if(accuracy > accuracy_max):
+            accuracy_max = accuracy
+            clf = clf_temp
+
+    out = "Classification_input/" + filename +'_tree'+str(i)+'.dot'
+    tree.export_graphviz(clf,out_file=out,feature_names=header)
+    print_out ='accuracy ' + filename +'_tree'+str(i) + ': '
+    print_out = print_out + str(accuracy_max)
+    print(print_out)
+    accuracy_max = 0
+
+print("\n The program generates three trees with highest accuracy. Please run: dot -Tpng " + filename +"_treen.dot" + " -o tree.png to see the final decision tree. Please note that treen is tree0, tree1, or tree2. \n")
---
+++ Cluster.py
@@ -0,0 +1,133 @@
+# Clustering and other steps :)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use("ggplot")
+from sklearn.cluster import KMeans
+from scipy.stats import norm
+
+filename = input("Enter the name of your input data set (.csv) without file type: ")
+print("\n **Please make sure your data set include id feature** \n")
+measurements = input("Are 10-measurements included in file as features T1 ... T10 (yes(y)/no(n))? ")
+print("\n **In case of \'No\', you should put *mean* and *std* (standard deviation) for each record. Header of features should be mean and std respectfully** \n")
+cluster_num= input("Enter number of clusters to divide data set (default is 2): ")
+if(cluster_num == ""):
+    cluster_numbers = 2
+else:
+    try:
+        cluster_numbers = int(cluster_num)
+    except ValueError:
+        print("That's not an int!")
+
+cluster_image = input("Enter the name of plot file for clustering: ")
+cluster_output = input("Enter the name of output data set (.csv) file without file type: ")
+
+df = pd.read_csv("Clustering_input/" + filename+".csv")
+
+if(measurements == "yes" or measurements == "y" or measurements == ""):
+    df_T = df[['T1','T2','T3','T4','T5','T6','T7','T8','T9','T10']]
+    df['mean'] = (df['T1'] + df['T2'] + df['T3'] + df['T4'] + df['T5'] + df['T6'] + df['T7'] + df['T8'] + df['T9'] + df['T10'])/(10)
+    std = np.array(df_T).std(1)
+    df['std'] = pd.DataFrame(std)
+
+Mean = np.array(df['mean'].reshape(-1,1))
+np.round(Mean,2)
+ST_DIV = np.array(df['std'].reshape(-1,1))
+np.round(ST_DIV,2)
+
+kmeans = KMeans(n_clusters=cluster_numbers)
+kmeans.fit(Mean)
+
+centroids = kmeans.cluster_centers_
+labels = kmeans.labels_
+
+if(cluster_numbers < 8):
+    colors = ["g.","r.","c.","y.","b.","k.","m."]
+else:
+    colors = cluster_numbers*["g.","r.","c.","y.","b.","k.","m."]
+
+for i in range(len(Mean)):
+    plt.plot(i, Mean[i][0], colors[labels[i]], markersize = 10)
+
+plt.savefig("Clustering_results/" + cluster_image+".png")
+print("Cluster plot has generated. Please wait to generate final data set for Classification step. We are going to calculate weight and label for each record!")
+
+label_set = set(labels)
+
+myMin = []
+myMax = []
+flag = True
+for set in label_set:
+    for i in range(len(Mean)):
+        if labels[i]==set:
+            if flag:
+                min,max = Mean[i][0],Mean[i][0]
+                flag = False
+            else:
+                temp = Mean[i][0]
+                if(temp < min):
+                    min = Mean[i][0]
+                if(temp > max):
+                    max = Mean[i][0]
+    myMin.append(min)
+    myMax.append(max)
+    flag = True
+
+minNP = np.array(myMin)
+maxNP = np.array(myMax)
+
+minNP = np.sort(minNP)
+maxNP = np.sort(maxNP)
+minNP = np.delete(minNP,0,0)
+maxNP = np.delete(maxNP,-1,0)
+
+intervals = [0]
+for i in range(len(minNP)):
+    temp = (minNP[i] + maxNP[i])/2
+    intervals.append(temp)
+intervals.append(intervals[-1]*1000)
+
+rows, columns = len(Mean), cluster_numbers
+Clusters = [[0 for x in range(columns)] for y in range(rows)]
+
+if(measurements == "yes" or measurements == "y" or measurements == ""):
+    df.drop(['T1','T2','T3','T4','T5','T6','T7','T8','T9','T10','std','mean'],1,inplace=True)
+else:
+    df.drop(['std','mean'],1,inplace=True)
+
+header = list(df.columns.values)
+X = np.array(df)
+m, n = X.shape
+X1 = np.zeros((m*cluster_numbers,n+2))
+new_rows = 0
+for i in range(len(Mean)):
+    for j in range(len(intervals)-1):
+        interval1 = intervals[j]
+        interval2 = intervals[j+1]
+        Clusters[i][j] = norm(Mean[i][0],ST_DIV[i][0]).cdf(interval2) - norm(Mean[i][0],ST_DIV[i][0]).cdf(interval1)
+        Clusters[i][j] = round(Clusters[i][j],2)
+        if(Clusters[i][j] > 0.99):
+            Clusters[i][j] = 1
+        elif(Clusters[i][j] < 0.01):
+            Clusters[i][j] = 0
+    for k in range(cluster_numbers):
+        if(Clusters[i][k] > 0):
+            X1[new_rows,:n] = X[i,:]
+            X1[new_rows,n] = Clusters[i][k] * 100
+            X1[new_rows,n+1] = k
+            new_rows += 1
+
+header.append('weight')
+header.append('label')
+
+i = 0
+while i < len(X1):
+    if(X1[i,n] == 0.00):
+        X1 = X1[~np.all(X1 == 0, axis=1)]
+    else:
+        i += 1
+df2 = pd.DataFrame(X1, columns=header)
+df2.set_index('id', inplace=True)
+
+df2.to_csv("Clustering_results/" + cluster_output+".csv")

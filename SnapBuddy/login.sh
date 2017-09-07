b8758e244a3606938581d12cb6df04d4e15f89a6
Saeid Tizpaz Niari <saeid.tizpazniari@colorado.edu>
2017-09-07 17:27:21 -0600

    some unnecessary files are removed

--- SnapBuddy/login.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -s -c cookies.txt -F username=$1 -F password=$2 --insecure https://localhost:8080/login
--- SnapBuddy/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./bin/snapbuddyhost_1 -p 8080 -d data/ -k ServersPrivateKey.txt -w ServersPasswordKey.txt
